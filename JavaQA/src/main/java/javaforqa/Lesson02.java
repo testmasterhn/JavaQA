e649d753c81f150a0add66c5abbf295af21c8c6a
khanhtran86 <khanh.tx@live.com>
2019-01-05 10:10:15 +0700

    add class 03

--- JavaQA/src/main/java/javaforqa/Lesson02.java
+++ JavaQA/src/main/java/javaforqa/Lesson02.java
@@ -8,10 +8,12 @@ import org.junit.Test;
 
 public class Lesson02 {
 	/*
-	 * Bài 01: Viết class MyNumber với các đặc điểm sau - Thuộc tính: int[]
-	 * arrNumber - lưu trữ một mảng các số nguyên - Phương thức: + MyNumber(int[]
-	 * numbers) - Khởi tạo giá trị cho mảng số nguyên arrNumber +
-	 * PrintPrimeNumbers() - In cá số nguyên tố có trong mảng arrNumber ra màn hình
+	 * Bài 01: Viết class MyNumber với các đặc điểm sau 
+	 * - Thuộc tính: 
+	 * int[] arrNumber - lưu trữ một mảng các số nguyên 
+	 * - Phương thức: 
+	 * + MyNumber(int[] numbers) - Khởi tạo giá trị cho mảng số nguyên arrNumber 
+	 * + PrintPrimeNumbers() - In cá số nguyên tố có trong mảng arrNumber ra màn hình
 	 * + GetPrimeNumbers() - Trả ra một mảng các số nguyên tố từ mảng arrNumber
 	 * 
 	 * Bài 02: Viết Testmethod cho phương thức GetPrimeNumbers() ở bài 01
---
+++ JavaQA/src/main/java/javaforqa/Lesson03.java
@@ -0,0 +1,15 @@
+package javaforqa;
+
+public class Lesson03 {
+	/*
+	 * Bài 01: Viết class vẫn là MyNumber với các đặc điểm
+	 * - Thuộc tính: 
+	 * 		int[] arrNumber - lưu trữ một mảng các số nguyên 
+	 * - Phương thức: 
+	 * 		+ MyNumber(int[] numbers) - Khởi tạo giá trị cho mảng số nguyên arrNumber 
+	 * 		+ GetPrimeNumbers() - Trả ra một mảng các số nguyên tố từ mảng arrNumber
+	 * 		+ GetSummary() - Trả ra tổng các số nguyên tố tìm được
+	 * 
+	 * Bài 02: Viết Test method cho 2 methods  GetPrimeNumbers và GetSummary ở trên
+	 */
+}
