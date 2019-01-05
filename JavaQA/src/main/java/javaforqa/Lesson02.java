223998506713fdf7daf552573f1c462d7400ef96
khanhtran86 <khanh.tx@live.com>
2019-01-05 08:24:26 +0700

    create lesson 02.java

---
+++ JavaQA/src/main/java/javaforqa/Lesson02.java
@@ -0,0 +1,14 @@
+package javaforqa;
+
+public class Lesson02 {
+	/*
+	 * Bài 01: Viết class MyNumber với các đặc điểm sau
+	 * - Thuộc tính: int[] arrNumber - lưu trữ một mảng các số nguyên
+	 * - Phương thức: 
+	 * 	 + MyNumber(int[] numbers) - Khởi tạo giá trị cho mảng số nguyên arrNumber
+	 *   + PrintPrimeNumbers() - In cá số nguyên tố có trong mảng arrNumber ra màn hình
+	 *   + GetPrimeNumbers() - Trả ra một mảng các số nguyên tố từ mảng arrNumber 
+	 *   
+	 * Bài 02: Viết Testmethod cho phương thức GetPrimeNumbers() ở bài 01
+	 */
+}
