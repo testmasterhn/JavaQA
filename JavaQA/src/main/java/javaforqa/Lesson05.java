f08ec08b37a350df345f756f160dc21cdeb5b063
khanhtran86 <khanh.tx@live.com>
2019-01-10 22:11:34 +0700

    Create lesson 05

---
+++ JavaQA/src/main/java/javaforqa/Lesson05.java
@@ -0,0 +1,14 @@
+package javaforqa;
+
+public class Lesson05 {
+/*
+ * Viết class MyArray với các đặc điểm sau:
+ * -Properties:
+ *  + int[] Numbers - Lưu trữ một mảng các số nguyên 
+ * -Methods:
+ *  + MyArray(int[] numbers) - Khởi tạo mảng các số nguyên cho thuộc tính Numbers
+ *  + CountNumber(int x) - Đêm số lần xuất hiện phần tử x trong mảng Numbers
+ *  + RemoveDupplicateNumber() - Phương thức loại bỏ các phần tử trùng trong mảng Numbers
+ *	Viết class Test kiểm thử cho method CountNumber, RemoveDupplicateNumber
+ */
+}
