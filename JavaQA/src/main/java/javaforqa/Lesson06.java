333a4ad10c941f01853a1e90b299394ccdfe31f1
khanhtran86 <khanh.tx@live.com>
2019-01-13 17:46:09 +0700

    Create Lesson 06

---
+++ JavaQA/src/main/java/javaforqa/Lesson06.java
@@ -0,0 +1,14 @@
+package javaforqa;
+
+public class Lesson06 {
+/*
+ * Tạo một class là MyNumber với các thuộc tính
+ * int[] Numbers;
+ * 
+ * Phương thức
+ * - MyNumber(int[] numbers) - Khởi tạo giá trị cho thuộc tính Numbers
+ * - GetMyNumber() - Trả ra những giá trị là số nguyên tố và không bị trùng trong mảng Numbers
+ * - GetTotal() - Trả ra tổng tất cả các giá trị số nguyên tố chẵn vừa tìm được.
+ * - GetLuckyNumber() - Tìm xem có số nguyên tố nào trong mảng Numbers có giá trị bằng tổng 02 số nt liền trước không? 
+ */
+}
