5e9a6d86021b46ba2b86b284940a2c10fd433335
khanhtran86 <khanh.tx@live.com>
2019-01-17 09:32:40 +0700

    Create lesson 07

---
+++ JavaQA/src/main/java/javaforqa/Lesson07.java
@@ -0,0 +1,14 @@
+package javaforqa;
+
+public class Lesson07 {
+/*
+ * Viết class MyNumber với các thuộc tính
+ * int[] ListNumber - Lưu trữ một danh sách các số được khởi tạo qua constructor
+ * Methods:
+ * - MyNumber(int[] number) - Khởi tạo giá trị cho thuộc tính ListNumber
+ * - GetMinNumber() - Trả ra giá trị nhỏ nhất trong ListNumber
+ * - GetMaxNumber() - Trả ra giá trị lớn nhất trong ListNumber
+ * - GetDistanceMinMax() - Đưa ra các số nằm giữa Min và Max
+ * Viết test cho 3 methods GetMinNumber, GetMaxNumber, GetDistanceMinMax
+ */
+}
